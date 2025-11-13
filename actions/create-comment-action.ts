c7ba6b9590c4ce298c6aa0c1a704b3ffb267c619
Camilo <camilox168@outlook.com>
2025-11-12 19:22:54 -0500

    fixing database connection

--- actions/create-comment-action.ts
+++ actions/create-comment-action.ts
@@ -4,11 +4,11 @@ import { prisma } from "../src/lib/prisma"
 
 export async function createComment(data) {
     if(!data) return console.log('Error al obtener los datos')
-
+        
     try {
         await prisma.comments.create({
             data: {
-                name: data.name, comment: data.message
+                name: data.name, comment: data.comment
             }
         })
     } catch (error) {
--- app/forum-comments/api/route.ts
+++ app/forum-comments/api/route.ts
@@ -2,6 +2,5 @@ import { prisma } from "../../../src/lib/prisma";
 
 export async function GET() {
     const comments = await prisma.comments.findMany()
-    console.log(comments)
     return Response.json(comments)
 }
